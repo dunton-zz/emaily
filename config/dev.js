a4676c424954c6496fd58508d686b5531df34cd6
Ryan Dunton <ryandunton1@gmail.com>
2018-10-02 22:25:26 -0400

    sending keys to cloud to transfer

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+// dev.js - DONT commit this
+module.exports = {
+  googleClientID:
+    "906500628571-khe4t91kl04vj9gm2tk9iulha672e4dv.apps.googleusercontent.com",
+  googleClientSecret: "zrS94c5fZoAS-9TSZTsEw5_I",
+  mongoURI: "mongodb://ryan:Gobroncos15@ds245772.mlab.com:45772/emaily-dev",
+  cookieKey: "teiosrtniresntn132yon412efintrsoetn324t90t",
+  facebookClientID: "334240960645843",
+  facebookClientSecret: "c190557ee8880ffb9e5c075029008228"
+};
