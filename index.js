8291ae46fddb01a96c096a878d4176e50300a850
Ryan Dunton <ryandunton1@gmail.com>
2018-10-21 19:13:58 -0400

    updates to route handling in production

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // EXPRESS will serve up production assets like our main.js or main.css file
+  app.use(express.static("client/build"));
+  // EXPRESS will serve up the index.html file if it doesnt recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFiled(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 // tells NODE to listen on Port 5000
 app.listen(PORT);
